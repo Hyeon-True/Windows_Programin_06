96e544af4b480aec78e946136a67ea28d9f9d0b8
Hyeon-True <leech8559162@naver.com>
2023-11-30 23:15:10 +0900

    Add to Play_List.cs and Option.cs

---
+++ Windows_Programing_06/Option.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows_Programing_06
+{
+    internal class Option
+    {
+        private DriverOptions initbrowser()
+        {
+            var option = new ChromeOptions();
+            option.AddArgument("headless");
+            return option;
+        }
+    }
+}
---
+++ Windows_Programing_06/Play_List.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows_Programing_06
+{
+    internal class Play_List
+    {
+        public static Play_List playlist = new Play_List();
+
+        private ArrayList play_list;
+        private ArrayList play_list_title;
+
+        public Play_List()
+        {
+            play_list = new ArrayList();
+            play_list_title = new ArrayList();
+        }
+
+        public void Add_Play_List(String url)
+        {
+            this.play_list.Add(url);
+        }
+
+        public void Add_Play_List_Title(String title)
+        {
+            this.play_list_title.Add(title);
+        }
+
+        public ArrayList Get_Play_List()
+        {
+            return this.play_list;
+        }
+
+        public ArrayList Get_Play_List_Title()
+        {
+            return this.play_list_title;
+        }
+
+    }
+}
